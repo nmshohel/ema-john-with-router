e11e8a7aa2e104793f8c6596a66e827784c2ef02
nmshohel <nmshohel1992@gmail.com>
2021-10-04 01:54:38 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+import { BrowserRouter,Switch,Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import OrderReview from './components/OrderReview/OrderReview';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import PlaceOrder from './components/PlaceOrder/PlaceOrder';
 
 function App() {
   return (
     <div>
+    
+      <BrowserRouter>
       <Header></Header>
-      <Shop></Shop>
+        <Switch>
+          <Route exact path="/">
+              <Shop></Shop>
+          </Route>
+          <Route path="/shop">
+              <Shop></Shop>
+          </Route>
+          <Route path="/review">
+            <OrderReview>
+
+            </OrderReview>
+
+          </Route>
+          <Route path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route path="/placeorder">
+            <PlaceOrder></PlaceOrder>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+
+     
     </div>
   );
 }
--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link,useHistory } from 'react-router-dom';
+import { clearTheCart, removeFromDb } from '../../utilities/fakedb';
+import Cart from '../Cart/Cart';
+import useCart from '../hooks/useCard';
+import useProducts from '../hooks/useProduct';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const OrderReview = () => {
+    const [products,setproducts]=useProducts();
+    const [cart, setCart]=useCart(products);
+    const history=useHistory();
+    const handleRemove=key=>{
+        const newCart=cart.filter(product=>product.key!==key);
+        setCart(newCart);
+        removeFromDb(key);
+    }
+    const handleOrderReview=()=>{
+        history.push('/placeorder');
+        setCart([]);
+        clearTheCart();
+    }
+   
     return (
-        <div>
-            <h2>This is Order Review</h2>
+        <div className="shop-container">
+            <div className="product-container">
+                {
+                    cart.map(product=><ReviewItem
+                    key={product.key}
+                    product={product}
+                    handleRemove={handleRemove}
+                    ></ReviewItem>)
+                }
+
+            </div>
+            <div className="cart-container">
+                    <Cart cart={cart}>
+                        <Link>
+                        <button onClick={handleOrderReview} className="btn-regular">Place Order</button>
+                        </Link>
+                    </Cart>
+            </div>
+
+           
         </div>
     );
 };
